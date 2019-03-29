51a5972ac1590612af5aeff7b2d43a2759a1d5e3
joaomgcd <support@joaoapps.com>
2019-03-29 13:04:17 +0000

    Renamed to FCMClientImplementation

--- fcm-client.js
+++ fcm-client.js
@@ -1,11 +1,10 @@
 //EDIT THIS FILE
 
 /***
-* Name the FCMClientTest whatever you want but make sure it extends FCMClient
 * Make sure to call the super() constructor with your sender ids
 * When your website is closed the handleBackgroundMessage will be called. You should create a notification there.
 ***/
-class FCMClientTest extends FCMClient{
+class FCMClientImplementation extends FCMClient{
 	constructor(){
 		super(["YOUR_SENDER_ID"])
 	}
--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,6 +2,6 @@ self.importScripts('https://www.gstatic.com/firebasejs/5.9.1/firebase-app.js');
 self.importScripts('https://www.gstatic.com/firebasejs/5.9.1/firebase-messaging.js');
 self.importScripts('/fcm.js');
 self.importScripts('/fcm-client.js');
-const fcmClient = new FCMClientTest();
+const fcmClient = new FCMClientImplementation();
 fcmClient.initServiceWorker(self);
 
